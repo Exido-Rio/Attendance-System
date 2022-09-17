682116223708afe7e8b7a7d39569532520ecec0a
Exido Rio <76099242+Exido-Rio@users.noreply.github.com>
2022-09-17 12:36:37 +0530

    Add files via upload

---
+++ entry.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+import face_recognition
+import os 
+from time import process_time
+from datetime import datetime
+
+
+t1 = process_time()
+loc ='images' # images file location
+images = [] # list of images
+peopele =[] # name of people 
+mylist  = os.listdir(loc)
+home = os.getcwd()
+existing_ppl = []
+
+# To get the list of csv files in Data dir except Names.csv one 
+os.chdir('Data')
+for file in os.listdir():
+    if file =='Names.csv':
+        continue
+    if os.path.isfile(file):
+        file = file.split(".")[-2]
+        existing_ppl.append(file)
+os.chdir(home)
+
+
+
+for ts_img in mylist : # used to detect images files inside the images directory
+    curr_img = cv2.imread(f'{loc}/{ts_img}')
+    images.append(curr_img)
+    peopele.append(os.path.splitext(ts_img)[0])
+
+print("list people who's data is stored in data.csv ",existing_ppl)
+
+def facedata(images):
+    new_enrty=[]
+    os.chdir("Data")
+    with open('Names.csv', 'r+') as f:
+        myCsvList = f.readlines()
+        peoplename = []
+        for line in myCsvList:
+            entry = line.split(',')
+            peoplename.append(entry[0])
+        for img,ppl in zip(images,peopele) :
+            if ppl not in existing_ppl and ppl not in peoplename :
+                new_enrty.append(ppl)
+                time_now = datetime.now()
+                current_time = time_now.strftime('%H:%M:%S')
+                date_today = time_now.strftime('%d/%m/%Y')
+                f.writelines(f'{ppl},{current_time},{date_today}\n')
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                encoded = face_recognition.face_encodings(img)[0]
+                np.savetxt(f"{ppl}.csv", encoded, delimiter = ",") 
+        print("List of new people added",new_enrty)     
+    os.chdir(home)
+
+facedata(images)
+
+t2 = process_time()
+print("Encoding complted time took :",t2-t1)
---
+++ recog.py
@@ -0,0 +1,73 @@
+import cv2
+import face_recognition
+import numpy as np
+import os 
+from datetime import datetime
+from numpy import genfromtxt
+
+encoded_list=[]
+home = os.getcwd()
+datafiles = []
+people =[]
+
+def getdata():# To read and data form csv files
+    os.chdir('Data')
+    for file in  os.listdir():
+        if file =='Names.csv':
+            continue
+        if os.path.isfile(file):
+            datafiles.append(file)
+            file = file.split(".")[-2]
+            people.append(file)
+    
+    for file in datafiles:
+        my_data = genfromtxt(file, delimiter=',')
+        encoded_list.append(my_data)
+    
+    os.chdir(home)
+
+getdata()
+
+def attendance(name): # used for attendance
+    with open('Attendace.csv', 'r+') as f:
+        myCsvList = f.readlines()
+        peoplename = []
+        for line in myCsvList:
+            entry = line.split(',')
+            peoplename.append(entry[0])
+        if name not in peoplename: # to prevent the overwirting the attedance again and again
+            time_now = datetime.now()
+            current_time = time_now.strftime('%H:%M:%S')
+            date_today = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{current_time},{date_today}')
+
+cam = cv2.VideoCapture(0)
+
+while True : # To match th faces on cam with the avaible recongnition data 
+    ret , frame =cam.read()
+    faces = cv2.resize(frame, (0,0), None, 0.25,0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    faces_on_cam=face_recognition.face_locations(faces)
+    encode_on_cam = face_recognition.face_encodings(faces, faces_on_cam)
+    
+    
+    for encodeFace, faceLoc in zip(encode_on_cam, faces_on_cam):
+        matches = face_recognition.compare_faces(encoded_list, encodeFace)
+        faceDis = face_recognition.face_distance(encoded_list, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex] :
+            name = people[matchIndex].upper()
+            #print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 0.56, (255, 255, 255), 2)
+            attendance(name)
+    cv2.imshow('Watcher' , frame)
+    if cv2.waitKey(1) == 13:
+        break
+cam.release()
+cv2.destroyAllWindows()
